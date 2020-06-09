83d5cde9564d259f151f516c015d76779f907615
fthebault <fthebault@herculepro.com>
2020-06-09 22:53:24 +0200

    initial commit

---
+++ src/main/java/com/stage/cda/herculepro/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.stage.cda.herculepro.controller;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+	
+	@PostConstruct
+	private void init() {
+		//TODO: init code
+	}
+	
+	@PreDestroy
+    private void shutdown() {
+        //TODO: destroy code
+    }
+	
+	@RequestMapping(method = RequestMethod.GET, path = {"/index"})
+	public String goToIndexPage(ModelMap modelMap) {
+		return "index";
+	}
+}
